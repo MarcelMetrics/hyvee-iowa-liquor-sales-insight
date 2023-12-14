e431ecb6bddb005816bd60d9aa637f9e6c677593
marcel-zhang <marcelzhang2014@gmail.com>
2023-12-13 22:23:11 -0500

    Data Staging
    Staging the data on MySQL server and prepare for data modelling.

---
+++ 03_HyVee_Sales_Data_Staging.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS STG_HYVEE;
+CREATE DATABASE STG_HYVEE;
+
+DROP DATABASE IF EXISTS INT_HYVEE;
+CREATE DATABASE INT_HYVEE;
+
+USE STG_HYVEE;
+
+CREATE TABLE sales (
+    invoice_line_no BIGINT,
+    date DATE,
+    store INT,
+    name VARCHAR(255),
+    city VARCHAR(100),
+    zipcode INT,
+    county VARCHAR(100),
+    category INT,
+    category_name VARCHAR(255),
+    vendor_no INT,
+    vendor_name VARCHAR(255),
+    itemno INT,
+    im_desc VARCHAR(255),
+    state_bottle_cost DECIMAL(10, 2),
+    state_bottle_retail DECIMAL(10, 2),
+    sale_bottles INT,
+    revenue DECIMAL(10, 2),
+    store_format VARCHAR(255)
+);
+
+SET GLOBAL local_infile = 'ON';
+SHOW VARIABLES LIKE 'local_infile';
+
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+LOAD DATA 
+	LOCAL INFILE "D:/GitHub/hyvee-iowa-liquor-sales-insight/data/clean_hyvee.csv"
+	INTO TABLE sales
+	FIELDS TERMINATED BY ',' 
+	ENCLOSED BY '"' 
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS;
+    
+SELECT count(*) FROM sales;
+
+
+SHOW GLOBAL VARIABLES LIKE 'net_read_timeout';
+
+USE INT_HYVEE;
+
+CREATE TABLE INT_HYVEE.sales LIKE STG_HYVEE.sales;
+
+LOAD DATA 
+	LOCAL INFILE "D:/GitHub/hyvee-iowa-liquor-sales-insight/data/clean_hyvee.csv"
+	INTO TABLE sales
+	FIELDS TERMINATED BY ',' 
+	ENCLOSED BY '"' 
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS;
