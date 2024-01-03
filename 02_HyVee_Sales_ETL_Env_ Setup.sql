775ce148e8755ce9b5b7e6f6ce96b3479d0dd960
marcel-zhang <marcelzhang2014@gmail.com>
2024-01-03 18:48:38 -0500

    ETL Pipeline Environment Setup
    Setting up the environment for loading data into a MySQL server through a pipeline.

---
+++ 02_HyVee_Sales_ETL_Env_ Setup.sql
@@ -0,0 +1,42 @@
+-- Environment Setup
+
+START TRANSACTION;
+
+DROP DATABASE IF EXISTS STG_HYVEE;
+CREATE DATABASE STG_HYVEE;
+
+DROP DATABASE IF EXISTS INT_HYVEE;
+CREATE DATABASE INT_HYVEE;
+
+COMMIT;
+
+-- Staging Layer Setup
+
+USE STG_HYVEE;
+
+CREATE TABLE sales (
+    invoice_line_no VARCHAR(255),
+    date DATE,
+    store INT,
+    name VARCHAR(255),
+    city VARCHAR(100),
+    zipcode INT,
+    county VARCHAR(100),
+    category INT,
+    category_name VARCHAR(255),
+    vendor_no INT,
+    vendor_name VARCHAR(255),
+    itemno INT,
+    im_desc VARCHAR(255),
+    state_bottle_cost DECIMAL(10, 2),
+    state_bottle_retail DECIMAL(10, 2),
+    sale_bottles INT
+);
+
+SELECT count(*) FROM sales;
+
+-- Integration Layer Setup
+
+USE INT_HYVEE;
+
+CREATE TABLE INT_HYVEE.sales LIKE STG_HYVEE.sales;
